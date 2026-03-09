e9378526257c2f92509f5e67f37edb68f18a9408
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-03-09 15:10:29 +0900

    ✨ feat: スタイルを調整 (Components/Sample)

--- src/components/sample/Sample.tsx
+++ src/components/sample/Sample.tsx
@@ -3,6 +3,8 @@ import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
 import { Window } from "@/components/ui/window/Window";
 import { TitleBar } from "@/components/ui/titleBar/TitleBar";
+import styles from "./Sample.module.css";
+import classNames from "classnames";
 
 export const Sample: React.FC = () => {
   const [count, setCount] = useState(0);
@@ -10,30 +12,27 @@ export const Sample: React.FC = () => {
   return (
     <Window>
       <TitleBar icon="React">Sample</TitleBar>
-      <div className="flex justify-center gap-6">
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img
-            src={viteLogo}
-            className="h-32 p-6 transition-all hover:drop-shadow-2xl will-change-filter"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className="h-32 p-6 transition-all animate-logo-spin hover:drop-shadow-2xl will-change-filter"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h3 className="text-slate-700 text-6xl font-semibold mb-6">Vite + React</h3>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      <div className="flex flex-col">
+        <div className="flex justify-center gap-x-6 mb-6">
+          <a href="https://vite.dev" target="_blank" rel="noreferrer">
+            <img src={viteLogo} className={styles.logo} alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank" rel="noreferrer">
+            <img src={reactLogo} className={classNames(styles.logo, styles.react)} alt="React logo" />
+          </a>
+        </div>
+        <h3 className="text-slate-700 text-5xl font-semibold mb-12">Vite + React</h3>
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="self-center mb-5 px-5 py-2 rounded-lg border border-transparent bg-slate-100 text-slate-700 font-medium transition-colors duration-250 hover:border-blue-500 cursor-pointer"
+        >
+          count is {count}
+        </button>
         <p>
           Edit <code>src/components/sample/Sample.tsx</code> and save to test HMR
         </p>
+        <p className="mt-6 text-slate-400">Click on the Vite and React logos to learn more</p>
       </div>
-      <p className="text-gray-500">Click on the Vite and React logos to learn more</p>
     </Window>
   );
 };
