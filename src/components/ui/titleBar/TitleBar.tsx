55cf577dcf14fe2fb5657acd50244bb52bf01a7b
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-03-09 15:03:07 +0900

    ✨ feat: タイトルバーのアイコンサイズを調整

--- src/components/ui/titleBar/TitleBar.tsx
+++ src/components/ui/titleBar/TitleBar.tsx
@@ -8,7 +8,7 @@ interface TitleBarProps {
 
 export const TitleBar: React.FC<TitleBarProps> = ({ icon, children }) => (
   <h2 className="mb-6 -mx-8 -mt-8 px-2.5 py-2 bg-gradient-to-r from-slate-700 to-slate-600 text-white text-base font-semibold rounded-t-[inherit] text-left flex items-center gap-1.5">
-    {icon && <Icon name={icon} />}
+    {icon && <Icon name={icon} size={20} />}
     {children}
   </h2>
 );
