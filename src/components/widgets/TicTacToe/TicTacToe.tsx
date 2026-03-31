f1ff05a96dacf9c0577162f291732623795ee0b4
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-03-31 23:04:28 +0900

    ✨ feat: 履歴機能を実装

--- src/components/widgets/TicTacToe/TicTacToe.tsx
+++ src/components/widgets/TicTacToe/TicTacToe.tsx
@@ -79,22 +79,44 @@ const Board = ({ xIsNext, squares, onPlay }: BoardProps) => {
 
 const Game = () => {
   // 状態変数の宣言
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState<CellState[][]>([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares: CellState[]) => {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   };
 
+  const jumpTo = (nextMove: number) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((_, move) => {
+    let description: string;
+    if (move > 0) {
+      description = "Go to move #" + move;
+    } else {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)} className="rounded-xs border px-2 py-1 text-sm">
+          {description}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <>
-      <div>{/*TODO*/}</div>
-      <div className="mx-auto">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <div className="mx-auto">
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      <div>
+        <ol className="flex flex-col space-y-2">{moves}</ol>
       </div>
-    </>
+    </div>
   );
 };
 
