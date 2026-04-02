b10936ab561e7a4863c5de3046cd9fa0648bfb1d
tamaohome <85826245+tamaohome@users.noreply.github.com>
2026-04-02 23:39:06 +0900

    ✨ feat: タイトルバーに最小化ボタンを追加 (ui/TitleBar)

--- src/components/ui/TitleBar/TitleBar.tsx
+++ src/components/ui/TitleBar/TitleBar.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Icon } from "@/components/ui/Icon";
 import clsx from "clsx";
 
+import { FaCircle, FaMinusCircle } from "react-icons/fa";
+
+const MinumumButton = () => (
+  <div className="group pointer-events-auto absolute top-1/2 right-2 mr-1 h-3.5 w-3.5 -translate-y-1/2">
+    <FaCircle className="absolute inset-0 h-3.5 w-3.5 text-amber-300/80 opacity-100 transition-opacity duration-150 group-hover:opacity-0" />
+    <FaMinusCircle className="absolute inset-0 h-3.5 w-3.5 text-amber-300 opacity-0 transition-opacity duration-150 group-hover:opacity-100" />
+  </div>
+);
+
 export interface TitleBarProps {
   icon?: string;
   children: React.ReactNode;
@@ -10,7 +19,7 @@ export interface TitleBarProps {
 export function TitleBar({ icon, children }: TitleBarProps) {
   const classes = clsx(
     // ベースのスタイル
-    "text-white h-10 flex items-center gap-1.5 rounded-t-xs pl-3 ",
+    "relative text-white h-10 flex items-center gap-1.5 rounded-t-xs pl-3 pr-9",
     // 背景のスタイル
     "bg-linear-to-r from-black/45 to-black/35 backdrop-blur-sm",
   );
@@ -19,6 +28,7 @@ export function TitleBar({ icon, children }: TitleBarProps) {
     <h2 className={classes}>
       {icon && <Icon name={icon} size={20} />}
       {children}
+      <MinumumButton />
     </h2>
   );
 }
